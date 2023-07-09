6d9e3474c57b3b09b23a55b3eda474b8c0d1c2ab
Bastian Berrios Arancibia <bberriosarancibia@gmail.com>
2023-07-09 19:43:51 -0400

    Actualizado apuntes del DOM del curso de JavaScript

--- Curso JavaScript/06-DOM/app.js
+++ Curso JavaScript/06-DOM/app.js
@@ -79,5 +79,60 @@ for(var i = 0; i < impar.length; i++){
     par[i].style.backgroundColor = '#f4f4f4';
 }
 
+//ParentNode
+/*
+var itemList = document.querySelector('#items');
+console.log('ParentNode ');
+console.log(itemList.parentNode);
+var main = itemList.parentNode;
+main.style.backgroundColor = '#f4f4f4';
+console.log(main.parentNode.parentNode);
+*/
+
+//ParentElement
+var itemList2 = document.querySelector('#items');
+console.log('ParentElement ');
+console.log(itemList2.parentElement);
+var main2 = itemList2.parentElement;
+main2.style.backgroundColor = '#f4f4f4';
+console.log(main2.parentElement.parentElement);
+
+
+/*
+ParentNode y ParentElement:
+
+En JavaScript, tanto parentNode como parentElement son propiedades que se utilizan para
+acceder al nodo padre de un elemento en el árbol DOM (Document Object Model). 
+Sin embargo, hay una diferencia clave entre ellos:
+
+parentNode: Esta propiedad devuelve el nodo padre del elemento, independientemente de qué tipo de nodo sea. 
+Es decir, puede ser cualquier tipo de nodo, como un elemento, un texto, un comentario, etc. Si el elemento no tiene un nodo padre, 
+parentNode devolverá null.
+
+parentElement: Esta propiedad devuelve el elemento padre del elemento. Dicho de otra manera, solo se aplica a los nodos de tipo elemento. 
+Si el nodo padre es de un tipo diferente (como un nodo de texto o un comentario), se omite y se busca el siguiente nodo padre que sea de tipo elemento. 
+Si no hay un nodo padre de tipo elemento, parentElement devuelve null.
+
+*/
+
+
+//Elementos y Nodos Hijos
+var itemList3 = document.querySelector('#items');
+
+//ChildNodes
+console.log("ChildNodes");
+//console.log(itemList3.childNodes);
+
+//Children
+console.log("Children");
+//console.log(itemList3.children);
+
+//fistChild / firstElementChild
+console.log("fistChild / firstElementChild");
+//console.log(itemList3.firstElementChild);
+
+//lastChild / lastElementChild
+console.log("lastChild / lastElementChild");
+//console.log(itemList3.lastElementChild);
 
 
