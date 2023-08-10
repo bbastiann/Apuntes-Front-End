4a6cc853e7244bb93c214f0c852a81d416770f02
Bastian Berrios Arancibia <bberriosarancibia@gmail.com>
2023-08-09 20:17:59 -0400

    Apuntes Angular

--- Curso Fundamentos de Angular/Proyecto01/src/app/app.module.ts
+++ Curso Fundamentos de Angular/Proyecto01/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from './app.component';
 
 //Importamos el componente creado
 import {componenteComponent} from './components/componente/componente.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    componenteComponent //Importamos nuestro nuevo componente
+    componenteComponent, //Componente que creamos 
+    NavbarComponent,
+    DashboardComponent,
+    FooterComponent 
   ],
   imports: [
     BrowserModule
