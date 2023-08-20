89e80fd19fa5f9c094dbdb77610892ae3bafcb3c
Bastian Berrios Arancibia <bberriosarancibia@gmail.com>
2023-08-20 17:20:20 -0400

    Actualizados apuntes de Data Binding de Curso de Angular

--- Curso Fundamentos de Angular/Proyecto01/src/app/components/dashboard/dashboard.component.ts
+++ Curso Fundamentos de Angular/Proyecto01/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
+  //Variable de Property binging
+  urlImage: string;
+  textoPlaceHolder: string;
+  isDisabled: boolean;
+
+  //Variables de Event Binding
+  titulo: string;
+
+  constructor() {
+    this.urlImage = "https://w7.pngwing.com/pngs/201/90/png-transparent-logo-html-html5.png";
+    this.textoPlaceHolder = "Escribe Aqui.....";
+    this.isDisabled = false;
+
+    //Variables de Event Binding
+    this.titulo = "Event Binding"
+
+  }
+
+
+  ngOnInit(): void{}
+
+  getURL(): string{
+    return this.urlImage = "Alejandro";
+  }
+
+  cambiarTexto(): void{
+    this.titulo = "Esto es Event Binding";
+  }
 }
