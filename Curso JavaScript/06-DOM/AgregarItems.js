09027c4602320a3d8297cc9a202b1f3ac5d61dd2
Bastian Berrios Arancibia <bberriosarancibia@gmail.com>
2023-08-30 11:10:14 -0400

    Actualizacion apuntes de JavaScript

--- Curso JavaScript/06-DOM/AgregarItems.js
+++ Curso JavaScript/06-DOM/AgregarItems.js
@@ -2,9 +2,13 @@
 var form = document.getElementById('formAgregar');
 var lista = document.getElementById('items');
 
-
+//Evento Submit del Formulario
 form.addEventListener('submit',agregarItem);
 
+//Evento Click de la Lista
+lista.addEventListener('click', eliminarItem);
+
+//Funcion para agregar un Item a la Lista
 function agregarItem(e){
     e.preventDefault(); //Permite cancelar el evento si este es cancelable, sin detener el resto del funcionanmiento del evento.
     var newItem = document.getElementById('item').value; 
@@ -16,7 +20,7 @@ function agregarItem(e){
 
     //Creamos el Boton del Elemento
     var botonDel = document.createElement('button');
-    botonDel.className = 'btn btn-danger btn-sm float-right';
+    botonDel.className = 'btn btn-danger btn-sm float-right eliminar';
     botonDel.appendChild(document.createTextNode('X'));
 
     li.appendChild(botonDel);
@@ -24,4 +28,13 @@ function agregarItem(e){
 
     lista.appendChild(li);
 
+}
+
+//Funcion para Eliminar un Item de la Lista
+function eliminarItem(e){
+    if(e.target.classList.contains('eliminar'));
+    var li = e.target.parentElement;
+    console.log('Existe');
+    console.log(li);
+    lista.removeChild(li);
 }
