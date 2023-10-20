efda37c884ff1df1849c2d3c10a4043fa18b8fd9
Bastian Berrios Arancibia <bberriosarancibia@gmail.com>
2023-10-20 00:59:40 -0300

    Curso de JavaScript: Barra de Busqueda

--- Curso JavaScript/06-DOM/AgregarItems.js
+++ Curso JavaScript/06-DOM/AgregarItems.js
@@ -2,11 +2,16 @@
 var form = document.getElementById('formAgregar');
 var lista = document.getElementById('items');
 
+var filto = document.getElementById('filtro');
+
 //Evento Submit del Formulario
 form.addEventListener('submit',agregarItem);
 
 //Evento Click de la Lista
 lista.addEventListener('click', eliminarItem);
+
+//Evento del teclado en el campo de filtro
+filtro.addEventListener('keyup', filtrarItems);
 
 //Funcion para agregar un Item a la Lista
 function agregarItem(e){
@@ -37,4 +42,24 @@ function eliminarItem(e){
     console.log('Existe');
     console.log(li);
     lista.removeChild(li);
+}
+
+
+//Funciones para Filtrar elementos de la Lista
+function filtrarItems(e){
+    var texto = e.target.value.toLowerCase()
+    var items = lista.getElementsByTagName('li');
+
+    Array.from(items).forEach(function(item){
+
+        var itemNombre = item.firstChild.nodeValue;
+
+        if(itemNombre.toLowerCase().indexOf(texto) != -1){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none';
+        }
+    });
+
+    console.log(items);
 }
