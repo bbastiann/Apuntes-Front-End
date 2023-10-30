9b31327a00bf74d24e71b4f2abed3ce0d581c1d2
Bastian Berrios Arancibia <bberriosarancibia@gmail.com>
2023-10-29 22:33:03 -0300

    Curso de JavaScript: Destructuracion Parte 2

--- Curso JavaScript/07-ES6/app3.js
+++ Curso JavaScript/07-ES6/app3.js
@@ -25,6 +25,20 @@ console.log(Edad2);
 
 
 //Utilizando Alias
-const {Nombre3: n, Edad3: e} = persona;
+/*const {Nombre3: n, Edad3: e} = persona;
 console.log(n);
 console.log(e);
+*/
+
+
+//Ejemplo Realista
+
+const calcularMayoriaEdad = (year) => {
+    const edad = new Date().getFullYear() - year;
+    const mayoria = edad >= 18 ? true : false;
+    return [edad, mayoria];
+};
+
+const [edadEjem, mayoriaEjem] = calcularMayoriaEdad(1994);
+console.log(edadEjem);
+console.log(`Es Mayor de Edad: ${mayoriaEjem}`);
