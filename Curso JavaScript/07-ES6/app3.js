6a0c37bca3d2541bf16c62979d40101d5dddcaeb
Bastian Berrios Arancibia <bberriosarancibia@gmail.com>
2023-10-29 22:26:10 -0300

    Curso de JavaScript: Destructuracion Parte 1

---
+++ Curso JavaScript/07-ES6/app3.js
@@ -0,0 +1,30 @@
+//Destructuración - Destrucción - Destructuring
+
+
+//ES5
+var datos = ['Pablo' ,  30];
+
+//var nombre = datos[0];
+//var edad = datos[1];
+
+//ES6
+var [Nombre, Edad] =  ['Pedro', 35];
+//console.log(Nombre);
+//console.log(Edad);
+
+//  Ejemplo 2
+
+const persona = {
+    Nombre2: 'Carlos',
+    Edad2: 45
+};
+
+const {Nombre2, Edad2} = persona;
+console.log(Nombre2);
+console.log(Edad2);
+
+
+//Utilizando Alias
+const {Nombre3: n, Edad3: e} = persona;
+console.log(n);
+console.log(e);
