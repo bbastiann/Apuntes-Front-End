e7f3c36064c6a3db8382d6c03d3fdd5ab083b84a
Bastian Berrios Arancibia <bberriosarancibia@gmail.com>
2024-01-22 22:58:26 -0300

    Curso de JavaScript: Destructuracion de Objetos Parte 1

--- Curso JavaScript/08-Objetos/app4.js
+++ Curso JavaScript/08-Objetos/app4.js
@@ -2,7 +2,8 @@
  * Objeto String
  */
 
-//Documentacion: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String
+//Documentacion 
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String
 
 console.log("OBJETO STRING");
 
@@ -14,4 +15,30 @@ console.log(nombre.toLocaleLowerCase()); //este es un metodo
 console.log(nombre.toLocaleUpperCase());
 console.log(nombre.trim()) //eliminaremos el espacio del inicio y el final
 console.log(clave.includes('claves')); //verifica si la cadena esta dentro / Devuelve TRUE or FALSE
+
+
+/****Destructuración de Objetos***/
+
+//Nos permite extraer datos, ya sea objetos o arreglos
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+
+const respuesta = {
+    codigo: 200,
+    dato:{
+        persona: {
+            nombre: 'Pablo',
+            direccion : {
+                ciudad: 'Lima',
+                pais: 'Peru'
+            }
+             
+        }
+    }
+}
+
+let { codigo } = respuesta;
+
+console.log(respuesta);
+console.log(codigo);
+
 
