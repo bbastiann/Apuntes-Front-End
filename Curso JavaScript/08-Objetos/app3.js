5e51bd15ff723cc2a89c4c52f18a63f266c23da7
Bastian Berrios Arancibia <bberriosarancibia@gmail.com>
2024-01-22 22:17:22 -0300

    Curso de JavaScript: Uso de This

--- Curso JavaScript/08-Objetos/app3.js
+++ Curso JavaScript/08-Objetos/app3.js
@@ -3,14 +3,20 @@ console.log("Metodos");
 
 let personaA = {
     nombre: 'Pablo',
-    edad: '30',
+    edad: 30,
     sueldo: 30000,
 
     MetodoPrueba: function(){
         return 'Resultado desde Prueba';
+    },
+
+    CambiarEdad: function(e){
+        this.edad = this.edad + e;
     }
 }
 
 
 let result = personaA.MetodoPrueba();
 console.log(result);
+personaA.CambiarEdad(3);
+console.log(personaA.edad);
