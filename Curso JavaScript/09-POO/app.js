8c57ed28c1b0935dfdbf32c96baf3214c271a205
Bastian Berrios Arancibia <bberriosarancibia@gmail.com>
2024-01-24 09:42:03 -0300

    Curso de JavaScript: Getter y Setter

--- Curso JavaScript/09-POO/app.js
+++ Curso JavaScript/09-POO/app.js
@@ -1,6 +1,6 @@
-/******
- * Manejo de Clases con JavaScript
- */
+// /******
+//  * Manejo de Clases con JavaScript
+//  */
 
 class Persona{
     constructor(nombre, edad, profesiones = []){
@@ -22,24 +22,61 @@ class Persona{
     }
 }
 
-//Es necesario tener un metodo constructor para que se tome como clase 
-// o sino se considerara como un metodo cualquiera
+// //Es necesario tener un metodo constructor para que se tome como clase 
+// // o sino se considerara como un metodo cualquiera
 
-const persona1 = new Persona('Carlos', 25, ['Tecnico, Arquitecto']);
-const persona2 = new Persona('Maria', 45, ['Doctor']);
+// const persona1 = new Persona('Carlos', 25, ['Tecnico, Arquitecto']);
+// const persona2 = new Persona('Maria', 45, ['Doctor']);
 
-/*console.log(typeof persona1);
-console.log(persona1.nombre);
-console.log(persona2.edad);*/
+// /*console.log(typeof persona1);
+// console.log(persona1.nombre);
+// console.log(persona2.edad);*/
 
-console.log(persona1.GetBiografia());
-console.log(persona2.GetBiografia());
+// console.log(persona1.GetBiografia());
+// console.log(persona2.GetBiografia());
+
+// /****
+//  * SubClases
+//  */
+
+// //Una subclase es una clase hija de la clase padre
+
+// class Empleado extends Persona{
+//         constructor(nombre, edad, profesiones = [], sueldo, puesto){
+//             super(nombre, edad, profesiones); //necesitamos acceder al constructor de la clase padre para pasar los parametros usando 'super'
+//             this.sueldo = sueldo;
+//             this.puesto = puesto;
+//         }
+
+//         GetBiografia(){
+//             return super.GetBiografia() + ` Puesto: ${this.puesto}, Salario: ${this.sueldo}.`;
+//         }
+// }
+
+// const empleado1 = new Empleado('Bastian', 29, ['Ingeniero Civil Informatico'], 15000, 'Ingenieria');
+// const empleado2 = new Empleado('Ignacio', 29, ['Ingeniero Civil Informatico'], 10000, 'RRHH');
+
+// console.log(empleado1.GetBiografia());
+// console.log(empleado2.GetBiografia());
+
 
 /****
- * SubClases
+ * Getter y Setter
  */
 
-//Una subclase es una clase hija de la clase padre
+/*const datos = {
+    get ubicacion(){
+        return this._ubicacion;
+    },
+    set ubicacion(valor){
+        this._ubicacion = valor; 
+    }
+}
+
+datos.ubicacion = 'Santiago';
+console.log(datos.ubicacion);
+console.log(datos);*/
+
 
 class Empleado extends Persona{
         constructor(nombre, edad, profesiones = [], sueldo, puesto){
@@ -51,11 +88,19 @@ class Empleado extends Persona{
         GetBiografia(){
             return super.GetBiografia() + ` Puesto: ${this.puesto}, Salario: ${this.sueldo}.`;
         }
+
+        set sueldo(monto){
+            this._sueldo = monto;
+        }
+
+        get sueldo(){
+            return this._sueldo;
+        }
 }
 
 const empleado1 = new Empleado('Bastian', 29, ['Ingeniero Civil Informatico'], 15000, 'Ingenieria');
 const empleado2 = new Empleado('Ignacio', 29, ['Ingeniero Civil Informatico'], 10000, 'RRHH');
 
+empleado1.sueldo = 50000;
 console.log(empleado1.GetBiografia());
-console.log(empleado2.GetBiografia());
 
