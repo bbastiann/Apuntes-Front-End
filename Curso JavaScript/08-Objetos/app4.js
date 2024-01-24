e625f7ba1c4f33fe9971770deae46d4940a70a6e
Bastian Berrios Arancibia <bberriosarancibia@gmail.com>
2024-01-23 22:46:17 -0300

    Curso de JavaScript: Descontruccion de Objetos Parte 2

--- Curso JavaScript/08-Objetos/app4.js
+++ Curso JavaScript/08-Objetos/app4.js
@@ -22,23 +22,55 @@ console.log(clave.includes('claves')); //verifica si la cadena esta dentro / Dev
 //Nos permite extraer datos, ya sea objetos o arreglos
 // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
 
-const respuesta = {
-    codigo: 200,
-    dato:{
-        persona: {
-            nombre: 'Pablo',
-            direccion : {
-                ciudad: 'Lima',
-                pais: 'Peru'
+const GetPersonaDatos = () => {
+    const respuesta = [
+        {
+        codigo : 200,
+        data : {
+            persona : {
+                    nombre : 'Pablo',
+                    direccion : {
+                        ciudad : 'Lima',
+                        pais : 'Peru'
+                    }
+                }
             }
-             
+        },
+        {
+            codigo : 400,
+            data : {
+                persona : {
+                        nombre : 'Fernanda',
+                        direccion : {
+                            ciudad : 'Santiago',
+                            pais : 'Peru'
+                        }
+                    }
+            }
+        },
+        {
+            codigo : 600,
+              data : {
+                    persona : {
+                        nombre : 'Cristian',
+                        direccion : {
+                                ciudad : 'Temuco',
+                                pais : 'Peru'
+                            }
+                        }
+                    }
         }
-    }
+    ]
+    return respuesta;
 }
 
-let { codigo } = respuesta;
+let { codigo : status, data : { persona : { nombre : nom }} } = GetPersonaDatos()[0];
 
-console.log(respuesta);
-console.log(codigo);
+console.log(nom);
+
+for( const { codigo : status, data : { persona : { nombre : nom }} } of GetPersonaDatos()){
+    console.log(nom);
+}
+
 
 
