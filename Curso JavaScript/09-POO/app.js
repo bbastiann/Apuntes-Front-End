2b4389b57ed13d06a3824592247079bcb077e5a4
Bastian Berrios Arancibia <bberriosarancibia@gmail.com>
2024-01-24 09:07:50 -0300

    Curso de JavaScript: POO - Subclases pt 1

--- Curso JavaScript/09-POO/app.js
+++ Curso JavaScript/09-POO/app.js
@@ -3,10 +3,10 @@
  */
 
 class Persona{
-    constructor(nombre, edad, trabajos = []){
+    constructor(nombre, edad, profesiones = []){
         this.nombre = 'Pablo',
         this.edad = 30;
-        this.trabajos = trabajos;
+        this.profesiones = profesiones;
     }
 
     //Cuando creamos metodos dentro de una clase no es necesario
@@ -14,9 +14,9 @@ class Persona{
     GetBiografia(){
 
         //template string
-        let biografia = `${this.nombre}, tiene ${this.edad}. Trabajo: `;
-        this.trabajos.forEach((trabajo) => {
-            biografia += `${trabajo}, `;
+        let biografia = `${this.nombre}, tiene ${this.edad}. Profesiones: `;
+        this.profesiones.forEach((profesion) => {
+            biografia += `${profesion}, `;
         })
         return biografia;
     }
@@ -34,3 +34,24 @@ console.log(persona2.edad);*/
 
 console.log(persona1.GetBiografia());
 console.log(persona2.GetBiografia());
+
+/****
+ * SubClases
+ */
+
+//Una subclase es una clase hija de la clase padre
+
+class Empleado extends Persona{
+        constructor(nombre, edad, profesiones = [], sueldo, pusto){
+            super(nombre, edad, profesiones); //necesitamos acceder al constructor de la clase padre para pasar los parametros usando 'super'
+            this.sueldo = sueldo;
+            this.puesto = puesto;
+        }
+}
+
+const empleado1 = new Persona('Bastian', 29, ['Ingeniero Civil Informatico'], 15000, 'ingenieria');
+const empleado2 = new Persona('Ignacio', 29, ['Ingeniero Civil Informatico'], 10000, 'RRHH');
+
+console.log(empleado1.GetBiografia());
+console.log(empleado2.GetBiografia());
+
