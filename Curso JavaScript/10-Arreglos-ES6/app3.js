3312cccdc4ccf8379c854be8688ecfa4c1d02e6f
Bastian Berrios Arancibia <bberriosarancibia@gmail.com>
2024-01-30 00:18:54 -0300

    Curso de JavaScript: Arreglos - Ordenamiento utilizando Sort()

--- Curso JavaScript/10-Arreglos-ES6/app3.js
+++ Curso JavaScript/10-Arreglos-ES6/app3.js
@@ -32,3 +32,23 @@ const FiltrarIngredientes = ceviche4.filter(function(ing, index){
 })
 
 console.log(FiltrarIngredientes);
+
+
+/**
+ * Ordenamiento utilizando SORT()
+ */
+console.log("Ordenamiento utilizando SORT()");
+
+ceviche4.sort(function(a,b){
+    if(a.titulo > b.titulo){
+        return 1;
+    }
+    if(a.titulo < b.titulo){
+        return -1;
+    }
+    return 0;
+});
+
+
+console.log(ceviche4);
+
